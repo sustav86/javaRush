3059b4ead378ab4ba7aa371c30ceeb56180d5f2e
Sustavov Anton <sustav86@gmail.com>
2016-11-13 17:31:29 +0200

    clone()

--- com/javarush/test/level21/lesson08/task03/Solution.java
+++ com/javarush/test/level21/lesson08/task03/Solution.java
@@ -23,9 +23,14 @@ public class Solution {
         public int getJ() {
             return j;
         }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
     }
 
-    public static class B extends A {
+    public static class B extends A implements Cloneable {
         private String name;
 
         public B(int i, int j, String name) {
@@ -36,11 +41,53 @@ public class Solution {
         public String getName() {
             return name;
         }
+
+        @Override
+        public B clone() throws CloneNotSupportedException {
+            throw new CloneNotSupportedException();
+        }
     }
 
     public static class C extends B {
         public C(int i, int j, String name) {
             super(i, j, name);
         }
+
+        @Override
+        public C clone() throws CloneNotSupportedException {
+            return new C(getI(), getJ(), getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        A a1 = new A(1, 2);
+        A clone = null;
+        try {
+            clone = (A)a1.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("a1 = " + a1.i + " " + a1.j);
+        System.out.println("clone = " + clone.i + " " + clone.j);
+
+        C c1 = new C(1, 2, "df");
+        C cloneC = null;
+        try {
+            cloneC = (C)c1.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("c1 = " + c1.getI() + " " + c1.getJ());
+        System.out.println("cloneC = " + cloneC.getI() + " " + clone.getJ());
+
+//        B b1 = new B(1, 2, "df");
+//        B cloneB = null;
+//        try {
+//            cloneB = b1.clone();
+//        } catch (CloneNotSupportedException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("b1 = " + b1.getI() + " " + b1.getJ());
+//        System.out.println("cloneB = " + cloneB.getI() + " " + cloneB.getJ());
     }
 }
